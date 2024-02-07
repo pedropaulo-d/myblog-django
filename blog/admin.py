fb00be9c6be18f9cda876be0f06788a6bfd7873c
Pedro Paulo <pedrobrpp5@gmail.com>
2024-02-07 16:44:52 -0400

    get_absolute_url + criando um campo readonly na admin do Django

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
+from blog.models import Category, Page, Post, Tag
 from django.contrib import admin
-from blog.models import Category, Tag, Page, Post
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from django_summernote.admin import SummernoteModelAdmin
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -25,6 +28,7 @@ class CategoryAdmin(admin.ModelAdmin):
         "slug": ('name',),
     }
 
+
 @admin.register(Page)
 class PageAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
@@ -39,6 +43,7 @@ class PageAdmin(SummernoteModelAdmin):
         "slug": ('title',),
     }
 
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
@@ -49,11 +54,25 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = 'category', 'is_published',
     list_editable = 'is_published',
     ordering = '-id',
-    readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+    readonly_fields = (
+        'created_at', 'updated_at', 'created_by', 'updated_by',
+        'link',
+    )
     prepopulated_fields = {
         "slug": ('title',),
     }
     autocomplete_fields = 'tags', 'category',
+
+    def link(self, obj):
+        if not obj.pk:
+            return '-'
+
+        url_do_post = obj.get_absolute_url()
+        safe_link = mark_safe(
+            f'<a target="_blank" href="{url_do_post}">Ver post</a>'
+        )
+
+        return safe_link
 
     def save_model(self, request, obj, form, change):
         if change:
