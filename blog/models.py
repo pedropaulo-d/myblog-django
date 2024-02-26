50687f837b66c8ef8b839df68ce002f2a141bc69
Pedro Paulo <pedrobrpp5@gmail.com>
2024-02-26 11:32:17 -0400

    Completando o model Page para páginas livres com HTML

--- blog/models.py
+++ blog/models.py
@@ -82,6 +82,11 @@ class Page(models.Model):
     def get_absolute_url(self):
         if not self.is_published:
             return reverse('blog:index')
+        return reverse('blog:page', args=(self.slug, ))
+    
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
         return reverse('blog:page', args=(self.slug,))
 
     def save(self, *args, **kwargs):
